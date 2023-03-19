f65a2313676a1099107d880d09371026843fd86a
Marc-Philipp Esser <m-esser@mail.de>
2023-03-19 19:10:55 +0100

    Remove unneeded classes

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,32 +1,7 @@
-"""
-create Pydantic models
-"""
-from typing import List
+""" Create Pydantic models """
+from typing import Any, Dict, List
 
 from pydantic import BaseModel, validator
-
-
-def must_be_non_negative(v: float) -> float:
-    """Check if the v is non-negative
-
-    Parameters
-    ----------
-    v : float
-        value
-
-    Returns
-    -------
-    float
-        v
-
-    Raises
-    ------
-    ValueError
-        Raises error when v is negative
-    """
-    if v < 0:
-        raise ValueError(f"{v} must be non-negative")
-    return v
 
 
 class Location(BaseModel):
@@ -46,26 +21,3 @@ class DomainSummaryRequestConfig(BaseModel):
     method: str = "SerpstatDomainProcedure.getDomainsInfo"
     domains: List[str] = ["shop.rewe.de", "www.edeka24.de"]
     database: str = "g_de"
-
-
-# class ProcessConfig(BaseModel):
-#     """Specify the parameters of the `process` flow"""
-
-#     drop_columns: List[str] = ["Id"]
-#     label: str = "Species"
-#     test_size: float = 0.3
-
-#     _validated_test_size = validator("test_size", allow_reuse=True)(
-#         must_be_non_negative
-#     )
-
-
-# class ModelParams(BaseModel):
-#     """Specify the parameters of the `train` flow"""
-
-#     C: List[float] = [0.1, 1, 10, 100, 1000]
-#     gamma: List[float] = [1, 0.1, 0.01, 0.001, 0.0001]
-
-#     _validated_fields = validator("*", allow_reuse=True, each_item=True)(
-#         must_be_non_negative
-#     )
