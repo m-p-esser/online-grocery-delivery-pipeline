013592bf9fe2e846ecdf5e240bf7f4522c3253a0
Marc-Philipp Esser <m-esser@mail.de>
2023-03-25 20:56:56 +0100

    Add type hints for output

--- src/etl/persist.py
+++ src/etl/persist.py
@@ -1,9 +1,11 @@
 """Python module related to persist data"""
 
 import json
-from io import BytesIO
 
+from prefect import get_run_logger
+from prefect_gcp.bigquery import bigquery_insert_stream
 from prefect_gcp.cloud_storage import GcsBucket
+from prefect_gcp.credentials import GcpCredentials
 
 
 def save_result(
@@ -37,3 +39,39 @@ def save_result(
         gcs_bucket = GcsBucket.load("gcs-bucket")
         with open(save_path, "rb") as f:
             gcs_bucket.upload_from_file_object(f, save_path)
+
+
+def insert_rows_to_bigquery_table(
+    records: list[dict], dataset_id: str, table_name: str
+) -> list:
+    """Inserts records into a BigQuery table using the bigquery_insert_stream operator.
+
+    Parameters
+    ----------
+    records : list[dict]
+        The records to insert into the BigQuery table
+    dataset_id : str
+        The name of the dataset
+    table_name : str
+        The name of the table
+    """
+
+    logger = get_run_logger()
+
+    # Load GCP credentials from the context
+    gcp_credentials = GcpCredentials.load("gcp-credentials")
+
+    # Insert the records into the BigQuery table
+    result = bigquery_insert_stream(
+        dataset=dataset_id,
+        table=table_name,
+        records=records,
+        gcp_credentials=gcp_credentials,
+    )
+
+    logger.info("INFO level log message")
+    logger.info(
+        f"Inserted API response to BigQuery here: {dataset_id}.{table_name}"
+    )
+
+    return result
