f5ec9f947d214015edfd1c0b9551ef81d57880d1
Marc-Philipp Esser <m-esser@mail.de>
2023-03-25 14:41:48 +0100

    Added logging capabilities

--- src/request_domain_summary.py
+++ src/request_domain_summary.py
@@ -1,10 +1,10 @@
 """Python script to request Domain Summary Endpoint"""
 
 import requests
-from prefect import flow, task
+from prefect import flow, get_run_logger, task
 
 from etl import persist, request
-from utils import config
+from utils.config import DomainSummaryRequestConfig
 
 
 @task(
@@ -13,14 +13,17 @@ from utils import config
     name="Request Domain Summary Endpoint",
     description="Request the Domain Summary Serpstat API endpoint (https://serpstat.com/api/412-summarnij-otchet-po-domenu-v4-serpstatdomainproceduregetdomainsinfo/",
 )
-def request_domain_summary_endpoint(
-    config: config.DomainSummaryRequestConfig(),
-):
+def request_domain_summary_endpoint(method: str, params: dict):
     """Request the Domain Summary Serpstat API endpoint"""
 
-    method = config.method
-    params = {"domains": config.domains, "database": config.database}
+    logger = get_run_logger()
+
     response = request.request_serpstats_api(method, params)
+
+    logger.info("INFO level log messages")
+    logger.info(f"Requested API endpoint: {method}")
+    logger.info(f"Using the following parameters: {params}")
+
     return response
 
 
@@ -28,29 +31,41 @@ def request_domain_summary_endpoint(
 def parse_domain_summary_endpoint_response(response: requests.Response):
     """Parse the response from the Domain Summary Serpstat API endpoint"""
 
+    logger = get_run_logger()
+
     response_json = request.parse_response(response)
+
+    logger.info("INFO level log messages")
+    logger.info("Parsed the following API Response:")
+    logger.info(response_json)
+
     return response_json
 
 
 @task
 def save_domain_summary_endpoint_result(
-    response_json: dict, save_location: str
+    response_json: dict, save_path: str, save_location: str
 ):
     """Save the result from the Domain Summary Serpstat API endpoint"""
-    persist.save_result(response_json, save_location, store_location="gcs")
+
+    logger = get_run_logger()
+
+    persist.save_result(response_json, save_path, save_location)
+
+    logger.info("INFO level log message")
+    logger.info(f"Saved API response ({save_location}) here: {save_path}")
 
 
 @flow
-def domain_summary_endpoint_request_flow(
-    config=config.DomainSummaryRequestConfig(),
-    save_location=config.Location().data_raw_domain_summary,
-):
+def domain_summary_endpoint_request_flow(config: DomainSummaryRequestConfig):
     """Flow to request the Domain Summary Serpstat API endpoint"""
 
-    response = request_domain_summary_endpoint(config)
+    response = request_domain_summary_endpoint(config.method, config.params)
     result = parse_domain_summary_endpoint_response(response)
-    save_domain_summary_endpoint_result(result, save_location)
+    save_domain_summary_endpoint_result(
+        result, config.save_path, config.save_location
+    )
 
 
 if __name__ == "__main__":
-    domain_summary_endpoint_request_flow()
+    domain_summary_endpoint_request_flow(config=DomainSummaryRequestConfig())
