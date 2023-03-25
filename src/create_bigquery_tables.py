34e85990bf78f69d8ba4a612cddd6d7e5bf89d02
Marc-Philipp Esser <m-esser@mail.de>
2023-03-25 15:33:03 +0100

    Add function to create Bigquery dataset. Modify function to create Bigquery tables to accept Datamodel from config

--- src/create_bigquery_tables.py
+++ src/create_bigquery_tables.py
@@ -1,41 +1,63 @@
 """ Module to create a BigQuery table from Schema"""
 
-import hydra
 from google.cloud import bigquery
 from prefect_gcp.credentials import GcpCredentials
 
-
-# Converts schema dictionary to BigQuery's expected format for job_config.schema
-def format_schema(schema):
-    formatted_schema = []
-    for row in schema:
-        formatted_schema.append(
-            bigquery.SchemaField(row["name"], row["type"], row["mode"])
-        )
-    return formatted_schema
+from utils.data_models import BigQuerySchema, DomainSummaryBigQuerySchema
 
 
-@hydra.main(config_path="conf/raw", config_name="domain_summary_schema")
-def create_table(config):
+def create_bigquery_dataset(dataset_id: str):
+    """
+    Creates a BigQuery Dataset.
 
+    Args:
+        dataset_id: The name of the dataset to be created.
+    """
     # Load Credentials and Config
     gcp_credentials = GcpCredentials.load("gcp-credentials")
     project_id = gcp_credentials.project
-    dataset_id = config.dataset_id
-    table_name = config.table_name
 
     # Init Client
     client = bigquery.Client(project=gcp_credentials.project)
 
-    # Prepare Schema from YAML format to BigQuery format
-    schema = format_schema(config.schema)
+    # Construct a full Dataset object to send to the API
+    dataset_ref = f"{project_id}.{dataset_id}"
+    dataset = bigquery.Dataset(dataset_ref)
 
-    # Create Table
+    # Make an API request to create dataset
+    dataset = client.create_dataset(dataset, exists_ok=True)
+    print(f"Created dataset {client.project}.{dataset.dataset_id}")
+
+
+def create_bigquery_table(biqquery_schema: BigQuerySchema):
+    """
+    Creates a BigQuery Table.
+
+    Args:
+        dataset_id: BiqQuerySchema Data Model.
+    """
+    # Load Credentials and Config
+    gcp_credentials = GcpCredentials.load("gcp-credentials")
+    project_id = gcp_credentials.project
+
+    # Init Client
+    client = bigquery.Client(project=gcp_credentials.project)
+
+    # Construct a full Table object to send to the API
+    dataset_id = biqquery_schema.dataset_id
+    table_name = biqquery_schema.table_name
     table_id = f"{project_id}.{dataset_id}.{table_name}"
-    table = bigquery.Table(table_id, schema)
-    table = client.create_table(table)  # Make an API request
+    table = bigquery.Table(table_id, biqquery_schema.schema_definition)
+
+    # Make an API request to create table
+    client.create_dataset(dataset_id, exists_ok=True)
+    table = client.create_table(table)
     print(f"Created table {project_id}.{dataset_id}.{table_name}")
 
 
 if __name__ == "__main__":
-    create_table()
+
+    # Create multiple dataset(s) and tables
+    domain_summary_bigquery_schema = DomainSummaryBigQuerySchema()
+    create_bigquery_dataset(domain_summary_bigquery_schema.dataset_id)
+    create_bigquery_table(domain_summary_bigquery_schema)
